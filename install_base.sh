01334d946f90638245dbcbfdc9c69063b3482019
Justs Zarins <j.zarins@epcc.ed.ac.uk>
2025-10-14 10:10:22 +0100

    Add parmetis 32bit installer

--- install_base.sh
+++ install_base.sh
@@ -15,4 +15,5 @@ bash install_scripts/install_adios2.sh
 bash install_scripts/install_petsc.sh
 bash install_scripts/install_parhip.sh
 bash install_scripts/install_parmetis.sh
+bash install_scripts/install_parmetis_32bit.sh
 bash install_scripts/install_rcm_f90.sh
