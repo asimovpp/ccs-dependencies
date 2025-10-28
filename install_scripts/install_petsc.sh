d41c1f379be689812e2167a9828354e6f1b3065d
Sebastien Lemaire <s.lemaire@epcc.ed.ac.uk>
2025-10-28 19:37:47 +0000

    handle single precision petsc build

--- install_scripts/install_petsc.sh
+++ install_scripts/install_petsc.sh
@@ -1,7 +1,7 @@
 set -e
 
 
-source setup_$ENV.sh
+echo PETSC precision: $PETSC_PRECISION
 
 INSTALL_DIR=$PETSC
 cd $BUILD_DIR
@@ -10,7 +10,7 @@ git clone --depth 1 --branch v$PETSC_VERSION https://github.com/petsc/petsc.git
 cd petsc
 export PETSC_DIR=$(pwd)
 
-./configure ${PETSC_OPT} --with-cc=${CC} --with-fc=${FC} --with-cxx=${CXX} --with-fortran-datatypes=1 --with-fortran-interfaces=1 --with-fortran-bindings=1 --with-fortran-kernels=1 --with-debugging=1 --prefix=$INSTALL_DIR
+./configure ${PETSC_OPT} --with-cc=${CC} --with-fc=${FC} --with-cxx=${CXX} --with-fortran-datatypes=1 --with-fortran-interfaces=1 --with-fortran-bindings=1 --with-fortran-kernels=1 --download-fblaslapack=yes --with-precision=${PETSC_PRECISION} --prefix=$INSTALL_DIR
 make -j 16
 make install
 
