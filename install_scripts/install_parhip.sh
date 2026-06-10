02dc21afc328a2a007a808aabb34923677a7dd69
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-06-10 17:02:11 +0100

    Change to parhip for cray PE

--- install_scripts/install_parhip.sh
+++ install_scripts/install_parhip.sh
@@ -6,6 +6,9 @@ cd $BUILD_DIR
 
 git clone --depth 1 --branch v$PARHIP_VERSION https://github.com/KaHIP/KaHIP.git parhip
 cd parhip
+
+sed -i '1i#include <cstdint>' lib/io/mmap_graph_io.h
+
 mkdir build
 cd build
 CC=${CC} cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
