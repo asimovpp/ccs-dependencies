ee24ba4f845f288d3a73bc90c25b286f2fa569af
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-08-24 15:31:39 +0100

    Automates changesto likwid make file -- untested for cray

--- install_scripts/install_likwid.sh
+++ install_scripts/install_likwid.sh
@@ -8,7 +8,11 @@ cd $BUILD_DIR
 wget http://ftp.fau.de/pub/likwid/likwid-$LIKWID_VERSION.tar.gz
 tar -xaf likwid-$LIKWID_VERSION.tar.gz
 cd likwid-$LIKWID_VERSION
-vim make/include_GCC.mk
+
+sed -i "s/FC  = ifx/FC  = ftn/" make/include_GCC.mk
+sed -i "/^FCFLAGS/d" make/include_GCC.mk
+sed -i "s/^#FCFLAGS/FCFLAGS/" make/include_GCC.mk
+
 make PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct
 make PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct install
 
