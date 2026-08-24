e4b210f7a512233793914106d13375127a227362
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-08-24 14:57:18 +0100

    Changes to HDF5 install script To use cmake as recommended for v2.0.0 onwards

--- install_scripts/install_hdf5.sh
+++ install_scripts/install_hdf5.sh
@@ -5,12 +5,23 @@ source ${PWD}/setup_${ENV}.sh
 INSTALL_DIR=$HDF5_ROOT
 cd $BUILD_DIR
 
-git clone --depth 1 --branch hdf5_$HDF5_VERSION https://github.com/HDFGroup/hdf5.git
+git clone --depth 1 --branch $HDF5_VERSION https://github.com/HDFGroup/hdf5.git
 cd hdf5
 
-./configure --enable-parallel --enable-subfiling-vfd=yes --prefix=$INSTALL_DIR
-make -j 16
-make install
+# ./configure --enable-parallel --enable-subfiling-vfd=yes --prefix=$INSTALL_DIR
 
-cd ..
+mkdir -p build && cd build
+# config
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DHDF5_ENABLE_PARALLEL=ON \
+      -DBUILD_SHARED_LIBS=ON \
+      -DHDF5_ENABLE_SUBFILING_VFD=ON \
+      ..
+
+# compile
+cmake --build . --config Release -j 16
+#install
+cmake --install . --prefix $INSTALL_DIR
+
+cd $BUILD_DIR
 rm -rf hdf5
