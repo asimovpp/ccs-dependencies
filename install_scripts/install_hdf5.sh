ca3ae6aa1833ce8b110585971b7208f09176a575
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-08-24 11:23:47 +0100

    Adds subfile support for hdf5 install

--- install_scripts/install_hdf5.sh
+++ install_scripts/install_hdf5.sh
@@ -8,7 +8,7 @@ cd $BUILD_DIR
 git clone --depth 1 --branch hdf5_$HDF5_VERSION https://github.com/HDFGroup/hdf5.git
 cd hdf5
 
-./configure --enable-parallel --prefix=$INSTALL_DIR
+./configure --enable-parallel --enable-subfiling-vfd=yes --prefix=$INSTALL_DIR
 make -j 16
 make install
 
