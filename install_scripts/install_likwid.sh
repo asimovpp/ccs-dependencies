320edde8ad937e90a64a73383683bcd58e071ac3
Rui Apóstolo <r.apostolo@epcc.ed.ac.uk>
2026-08-25 14:56:24 +0100

    Add support for cray compiler for likwid script

--- install_scripts/install_likwid.sh
+++ install_scripts/install_likwid.sh
@@ -5,15 +5,27 @@ source ${PWD}/setup_${ENV}.sh
 INSTALL_DIR=$LIKWID
 cd $BUILD_DIR
 
+# defaults
+export lik_cmp="GCC"
+export CC=gcc
+export FC=ifort
+export FCFLAGS="-module ./"
+
+if [ "$CMP" = "cray" ]; then
+	export lik_cmp=CLANG
+	export CC=cc
+	export FC=ftn
+	export FCFLAGS="-ef -J ./"
+elif [ "$CMP" = "gnu" ]; then
+	export lik_cmp=GCC
+	export CC=cc
+	export FC=ftn
+	export FCFLAGS="-J ./ -fsyntax-only"
+fi
+
 wget http://ftp.fau.de/pub/likwid/likwid-$LIKWID_VERSION.tar.gz
 tar -xaf likwid-$LIKWID_VERSION.tar.gz
 cd likwid-$LIKWID_VERSION
 
-sed -i "s/FC  = ifx/FC  = ftn/" make/include_GCC.mk
-sed -i "/^FCFLAGS/d" make/include_GCC.mk
-sed -i "s/^#FCFLAGS/FCFLAGS/" make/include_GCC.mk
-
-make PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct
-make PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct install
-
-
+make COMPILER=$lik_cmp CC=$CC FC=$FC FCFLAGS="$FCFLAGS" PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct
+make COMPILER=$lik_cmp CC=$CC FC=$FC FCFLAGS="$FCFLAGS" PREFIX=$INSTALL_DIR FORTRAN_INTERFACE=true ACCESSMODE=direct install
